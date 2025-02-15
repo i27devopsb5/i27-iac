f0194eaf17522fe349072b2b0e504e571a2c6d44
i27devopsb5 <i27devops+b5git@gmail.com>
2025-02-15 18:52:52 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -1,1 +1,10 @@
 # terraform resource to create kubernetes clusters
+resource "google_container_cluster" "primary" {
+  name               = "i27-cluster"
+  location           = "us-central1-a"
+  initial_node_count = 1
+  node_config {
+   machine_type = "e2-medium"
+   disk_size_gb = "30"
+  }
+}
